b387c146a766fca3219f37326b6a77f13ae83b73
Ali G.Jahed <aligjahed@gmail.com>
2023-01-25 12:53:23 +0330

    Add MedaitR

--- linkshortner-mvc-net/ConfigureServices.cs
+++ linkshortner-mvc-net/ConfigureServices.cs
@@ -1,11 +1,15 @@
 
+using System.Reflection;
+using MediatR;
+
 namespace linkshortner_mvc_net;
 
 public static class ConfigureServices
 {
-    public static IServiceCollection AddWebUI(this IServiceCollection services)
+    public static IServiceCollection AddWebUi(this IServiceCollection services)
     {
         services.AddControllersWithViews();
+        services.AddMediatR(Assembly.GetExecutingAssembly());
         return services;
     }
 }
