b75fb21af025849fa509484b9ab04169612c99ad
Ali G.Jahed <aligjahed@gmail.com>
2023-01-27 21:30:17 +0330

    Minor changes and fixes

--- linkshortner-mvc-net/wwwroot/js/site.js
+++ linkshortner-mvc-net/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Basic empty field form validation
+(() => {
+    'use strict'
 
-// Write your JavaScript code.
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
